48fb54283ca5d85a12fa611513be7d09c302c803
maojingjing <mjj0131@gmail.com>
2024-06-09 15:46:08 -0700

    learn alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,23 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        ZStack {
-//            LinearGradient(stops: [
-//                .init(color: .white, location: 0.45),
-//                .init(color: .black, location: 0.55)
-//            ], startPoint: .top, endPoint: .bottom)
-//            
-//            RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
-            
-//            AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .blue], center: .center)
-//
-            Text("Your content here")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .foregroundStyle(.white)
-                .background(.indigo.gradient)
-            
-            
+        Button("Show alert") {
+            showingAlert = true
+        }
+        .alert("Important Message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Please read this")
         }
     }
 }
